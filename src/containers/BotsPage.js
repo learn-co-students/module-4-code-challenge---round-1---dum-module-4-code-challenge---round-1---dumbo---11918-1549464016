024614a7cc02410078b3eadd82865fed76c36dcd
jriv95 <joel.rivera7995@gmail.com>
2019-02-06 11:19:47 -0500

    Close. It's a process though.

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import BotCollection from "./BotCollection";
-import YourBotArmy from "./YourBotArmy";
+import YourBotArmy from"./YourBotArmy";
 
 class BotsPage extends React.Component {
  
