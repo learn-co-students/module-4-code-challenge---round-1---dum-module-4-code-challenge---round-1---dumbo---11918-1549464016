04a0c2976414ade9cab9751d5f63c555680fffb6
github username <github email address>
2019-02-06 10:24:11 -0500

    rendered correct order

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -20,8 +20,8 @@ class BotsPage extends React.Component {
     //console.log(this.state.robots)
     return (
       <div>
-        <div>{ <BotCollection robots={ this.state.robots } /> }</div>
         <div>{ <YourBotArmy robots={this.state.robots}/> }</div>
+        <div>{ <BotCollection robots={ this.state.robots } /> }</div>
       </div>
     );
   }
