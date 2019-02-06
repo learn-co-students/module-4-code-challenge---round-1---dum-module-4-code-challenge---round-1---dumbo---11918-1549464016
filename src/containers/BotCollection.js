32b8342759f4525c6db374c8d35102a7453f5320
Thompson Plyler <nealok.g@gmail.com>
2019-02-06 10:03:11 -0500

    Robots found and placed on page.

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -5,10 +5,12 @@ class BotCollection extends React.Component {
   //your code here
 
   render(){
+		// console.log(this.props.bots)
+		let bots = this.props.bots.map(bot=> <BotCard bot={bot}/>)
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+    		  {bots}
     		  Collection of all bots
     		</div>
   	  </div>
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,24 @@
 import React from "react";
+import BotCollection from "../containers/BotCollection";
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  state={
+    bots:[]
+  }
+
+  componentDidMount(){
+    fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+      .then(r=>r.json())
+      .then(robots=> this.setState({
+        bots:robots}
+        ))
+      }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        {<BotCollection bots={this.state.bots}/>}
       </div>
     );
   }
@@ -14,3 +26,8 @@ class BotsPage extends React.Component {
 }
 
 export default BotsPage;
+
+
+//fetching from //fetching from : https://bot-battler-api.herokuapp.com/api/v1/bots
+
+//botspage --> botcollection --> botcard
