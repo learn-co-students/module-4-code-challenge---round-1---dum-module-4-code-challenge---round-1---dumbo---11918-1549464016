49ebca1d8147b5ac72205c3d9a45bcfe862beac5
Joseph <joeephus@gmail.com>
2019-02-06 10:13:04 -0500

    got the heirarchy done?

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -2,19 +2,20 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
-  //your code here
-	// renderBots = () => {
-	// 	let robots = this.props.robots.map((rob) => {
-	// 		return < BotCard key={rob.id} rob={rob}/>
-	// 	})
-	// 	return robots
-	// }
-  render(){
+
+	//your code here
+	renderBots = () => {
+			let robots = this.props.robots.map((rob) => {
+					return < BotCard key={rob.id} rob={rob} />
+				})
+				return robots
+			}
+			render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
     		</div>
-    		  <BotCard />
+    		  {this.renderBots()}
   	  </div>
   	);
   }
