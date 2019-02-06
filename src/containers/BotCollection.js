69df0932c8289081ecbb0294a9f2ac4909210ae7
Joseph <joeephus@gmail.com>
2019-02-06 10:36:17 -0500

    sorry i changed card to a class

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -4,18 +4,18 @@ import BotCard from "../components/BotCard";
 class BotCollection extends React.Component {
 
 	//your code here
-	renderBots = () => {
-			let robots = this.props.robots.map((rob) => {
-					return < BotCard key={rob.id} rob={rob} />
-				})
-				return robots
-			}
+	// renderBots = () => {
+	// 		let robots = this.props.robots.map((robots) => {
+	// 				return < BotCard key={robots.id} rob={robots} />
+	// 			})
+	// 			return robots
+	// 		}
 			render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
+    		  {this.props.robots.map((bot) => < BotCard key={bot.id} bot={bot} />)}
     		</div>
-    		  {this.renderBots()}
   	  </div>
   	);
   }
